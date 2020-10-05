1f2019938c5fefa9b6ecfc64afb4c2c3611b8d4e
Hanprogramer <30620423+Hanprogramer@users.noreply.github.com>
2020-10-05 09:59:37 +0700

    Created Addon Posts system

--- generate.py
+++ generate.py
@@ -13,24 +13,12 @@ f.close()
 TOOLS = os.listdir("posts/tools")
 ADDONS = os.listdir("posts/addons")
 
-f = open("index.html", "w")
-for line in content:
-    raw = line.strip()
-    if(raw == "<div class=\"GENERATED\"></div>"):
-        for tool in TOOLS:
-            if(not os.path.exists("posts/tools/%s/post.md" % tool)):
-                continue
-            # Genereate the posts
-            template = """
-                        <a class="post" href="posts/tools/%s/post.html">
-                            <img src="posts/tools/%s/thumb.png">
-                        </a>\n""" % (tool, tool)
-            f.write(template)
-
-            # Generate the individual post html
-            html = """<!DOCTYPE html>
+# Template header for all files
+header = """<!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../../../index.css">
         <!-- <link rel="stylesheet" href="../../../nav.css"> -->
         <link rel="icon" type="image/png" href="../../../logo.png">
@@ -44,23 +32,111 @@ for line in content:
         </script>
         <div class="navbar">
             <img class="round-icon" src="../../../favicon.ico">
-            <div class="titletext" href="https://youtube.com/Hanprogramer" target="_blank">
-                <a>Hanprogramer</a>
-                <a href="https://youtube.com/Hanprogramer" target="_blank" class="subtitle">youtube.com/Hanprogramer</a>
+            <div class="titletext">
+                <a href="../../../index.html">HANPROGRAMER</a>
+                <!-- <a href="https://youtube.com/Hanprogramer" target="_blank" class="subtitle">youtube.com/Hanprogramer</a> -->
             </div>
             <div style="flex-grow: 1"></div>
-            <button>Tools</button>
-            <button>Addons</button>
+            <button class="button" onclick="window.location = '../../../tools.html'">Tools</a>
+            <button class="button" onclick="window.location = '../../../addons.html'">Addons</a>
         </div>
 
         <div class="content">"""
-            html += (markdown2.markdown_path("posts/tools/%s/post.md" % tool))
-            html += """        </div>
+header_root = """<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="index.css">
+        <!-- <link rel="stylesheet" href="nav.css"> -->
+        <link rel="icon" type="image/png" href="logo.png">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script data-ad-client="ca-pub-9638544368327690" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <title>Hanprogramer</title>
+    </head>
+    <body>
+        <script>
+            CSS.paintWorklet.addModule('bubblePaint.js')
+        </script>
+        <div class="navbar">
+            <img class="round-icon" src="favicon.ico">
+            <div class="titletext">
+                <a href="index.html">HANPROGRAMER</a>
+                <!-- <a href="https://youtube.com/Hanprogramer" target="_blank" class="subtitle">youtube.com/Hanprogramer</a> -->
+            </div>
+            <div style="flex-grow: 1"></div>
+            <button class="button" onclick="window.location = './tools.html'">Tools</a>
+            <button class="button" onclick="window.location = './addons.html'">Addons</a>
+        </div>
+        <div class="content">
+        %s
+        <div class="post-container">"""
+footer = """        </div>
     </body>
 </html>"""
+f = open("index.html", "w")
+for line in content:
+    raw = line.strip()
+    if(raw == "<div class=\"GENERATED\"></div>"):
+        #########################################
+        #      TOOLS CONTENT GENERATOR
+        #########################################
+        f.write("<h1>Tools</h1>")
+        f.write('<div class="post-container">')
+        f_tool = open("tools.html", "w")
+        f_tool.write(header_root % ("<h1>Tools</h1>"))
+        for tool in TOOLS:
+            if(not os.path.exists("posts/tools/%s/post.md" % tool)):
+                continue
+            # Genereate the posts
+            template = """
+                        <a class="post" href="posts/tools/%s/post.html">
+                            <img src="posts/tools/%s/thumb.png">
+                        </a>\n""" % (tool, tool)
+            f.write(template)
+            f_tool.write(template)
+
+            # Generate the individual post html
+            html = header
+            html += (markdown2.markdown_path("posts/tools/%s/post.md" % tool))
+            html += footer
+
             post = open("posts/tools/%s/post.html" % tool, "w")
             post.write(html)
             post.close()
+        f.write('</div>')
+        f_tool.write(footer)
+        f_tool.close()
+        
+        #########################################
+        #      ADDONS CONTENT GENERATOR
+        #########################################
+        f.write("<h1>Addons</h1>")
+        f.write('<div class="post-container">')
+        f_tool = open("addons.html", "w")
+        f_tool.write(header_root % ("<h1>Addons</h1>"))
+        for tool in ADDONS:
+            if(not os.path.exists("posts/addons/%s/post.md" % tool)):
+                continue
+            # Genereate the posts
+            template = """
+                        <a class="post" href="posts/addons/%s/post.html">
+                            <img src="posts/addons/%s/thumb.png">
+                        </a>\n""" % (tool, tool)
+            f.write(template)
+            f_tool.write(template)
+
+            # Generate the individual post html
+            html = header
+            html += (markdown2.markdown_path("posts/addons/%s/post.md" % tool))
+            html += footer
+
+            post = open("posts/addons/%s/post.html" % tool, "w")
+            post.write(html)
+            post.close()
+        f.write('</div>')
+        f_tool.write(footer)
+        f_tool.close()
         continue
     f.write(line)
 f.close()
---
+++ posts/tools/mcpacker/post.md
@@ -0,0 +1,5 @@
+# MCPacker
+Mc Packer is a program used to make .mcaddon file which user can doubble click to install your addon.
+It can also merge packs together to a single click installation file (.mcpack)
+
+![](ss1.png) 
